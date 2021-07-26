f6f98a2385116fde16b286d4e3c2cbd2232f03cd
Selina-Knaepen <knaepen.selina@gmail.com>
2021-07-26 15:13:43 +0200

    Add allowance shows two decimals behind comma

--- app/src/main/java/be/huffle/allowanceplanner/ui/allowance/AllowanceViewModel.java
+++ app/src/main/java/be/huffle/allowanceplanner/ui/allowance/AllowanceViewModel.java
@@ -5,18 +5,20 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.text.DecimalFormat;
+import java.util.Currency;
+import java.util.Locale;
 
 public class AllowanceViewModel extends ViewModel
 {
 	private MutableLiveData<String> mText;
-	private float allowance = 0;
+	private double allowance = 0;
 	private DecimalFormat decimalFormat = new DecimalFormat();
 
 	public AllowanceViewModel()
 	{
 		decimalFormat.setMaximumFractionDigits(2);
 		mText = new MutableLiveData<>();
-		mText.setValue("€ " + allowance);
+		mText.setValue("€ " + decimalFormat.format(allowance));
 	}
 
 	public LiveData<String> getText()
